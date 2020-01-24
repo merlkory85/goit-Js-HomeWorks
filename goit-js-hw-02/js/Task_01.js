79f4b2eafd35fc70da7633ec936bb88dc23ac907
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-01-24 23:38:45 +0200

    add task_01

---
+++ goit-js-hw-02/js/Task_01.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const pets = ["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"];
+const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+let messege;
+
+const logItems = function(array) {
+  for (let i = 0; i < array.length; i += 1) {
+    const numArgument = i + 1;
+
+    console.log(`${numArgument} - ${array[i]}`);
+  }
+};
+
+// logItems(pets);
+logItems(numbers);
