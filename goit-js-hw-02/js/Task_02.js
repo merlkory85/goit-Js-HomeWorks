79879f66bd4eca87407e7e133f376963a46ecc46
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-01-25 13:21:04 +0200

    add Task_02

--- goit-js-hw-02/js/Task_02.js
+++ goit-js-hw-02/js/Task_02.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let message;
+let pricePerWord;
+let words;
+
+const calculateEngravingPrice = function(message, pricePerWord) {
+  const stringInArray = message.split(" ");
+  const arrayLength = stringInArray.length;
+  const result = arrayLength * pricePerWord;
+  console.log(result);
+};
+
+calculateEngravingPrice(
+  "Proin sociis natoque et magnis parturient montes mus", 390);
