481574c9b030428b870910432112b0dd3edfa3df
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-01-30 06:58:33 +0200

    make changies task04 task 05

--- goit-js-hw-01/js/Task_04.js
+++ goit-js-hw-01/js/Task_04.js
@@ -1,28 +1,18 @@
 "use strict";
 
-let credits = 23580;
+const credits = 23580;
 const pricePerDroid = 3000;
-const userRequest = Number(prompt("Какое количество дроидов вы хотите приобрести???"));
-const totalPrice = userRequest * pricePerDroid;
-const accountBalance = credits - totalPrice;
-const notANumber = Number.isNaN(userRequest);
+const userRequest = prompt("Сколько дроидов хотите купить");
+const totalPrice = Number(userRequest) * pricePerDroid;
+const restCredits = credits - totalPrice;
 let message;
-
-if (notANumber) {
-  message = "Вы ввели не число, попробуйте еще раз!!!";
-  } else {
-     message = `Вы купили ${userRequest} дроидов, на счету осталось ${accountBalance} кредитов D=)`;
-}
-
 if (userRequest === null) {
   message = "Отменено пользователем!";
-  // break;
-}
-
-if (totalPrice > credits) {
+} else if (totalPrice > credits) {
   message = "Недостаточно средств на счету!";
-  // break;
-  // } else {
-  //   message = `Вы купили ${userRequest} дроидов, на счету осталось ${accountBalance} кредитов D=)`;
+} else if (Number.isNaN(totalPrice)) {
+  message = "Вы ввели не число, попробуйте снова!!!";
+} else {
+  message = `Вы купили ${userRequest} дроидов, на счету осталось ${restCredits} кредитов.`;
 }
 console.log(message);
