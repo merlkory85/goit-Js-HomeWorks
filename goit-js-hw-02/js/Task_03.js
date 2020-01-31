52785ceea098b831a316988e510c47bbd24527a7
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-01-31 22:29:40 +0200

    add Task_03

--- goit-js-hw-02/js/Task_03.js
+++ goit-js-hw-02/js/Task_03.js
@@ -1,14 +1,21 @@
 "use strict";
 
 const findLongestWord = function(string) {
-  // твой код
+  let arrString = string.split(" ");
+  let longestWord = arrString[0];
+  for (let i = 1; i < arrString.length; i += 1) {
+    if (arrString[i].length > longestWord.length) {
+      longestWord = arrString[i];
+    }
+  }
+  return longestWord;
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
+
+
 console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
 
 console.log(findLongestWord('Google do a roll')); // 'Google'
 
 console.log(findLongestWord('May the force be with you')); // 'force'
+
