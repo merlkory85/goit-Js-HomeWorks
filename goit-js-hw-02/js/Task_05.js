b99be469eae1b4f20d030c8fd1793da6ac51695b
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-02-04 07:31:17 +0200

    add Task_05

--- goit-js-hw-02/js/Task_05.js
+++ goit-js-hw-02/js/Task_05.js
@@ -1,1 +1,25 @@
 "use strict";
+// const arrayString;
+let message;
+const spamToFind = "[spam]";
+const spamToFind2 = "sale";
+const checkForSpam = function(message) {
+  const arrayString = message.toLowerCase().split(" ");
+
+  for (const word of arrayString)
+    if (spamToFind === word) {
+      return "true";
+    } else if (spamToFind2 === word) {
+      return "true";
+    }
+
+  return "false";
+};
+
+console.log(checkForSpam("Latest technology news")); // false
+
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+
+console.log(checkForSpam("Get best sale offers now!")); // true
+
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
