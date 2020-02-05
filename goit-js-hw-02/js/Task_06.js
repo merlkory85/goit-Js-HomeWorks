42915c0b23b6aec94e4ff38923957218cb44fd8e
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-02-05 22:49:06 +0200

    add Task_06

--- goit-js-hw-02/js/Task_06.js
+++ goit-js-hw-02/js/Task_06.js
@@ -1,1 +1,28 @@
 "use strict";
+
+let input;
+const numbers = [];
+let total = 0;
+
+while (true) {
+  input = prompt("Введите число!");
+
+  if (input === null) {
+    alert("Отменено пользователем :(");
+    break;
+  }
+  input = Number(input);
+  const notANamber = Number.isNaN(input);
+
+  if (notANamber) {
+    alert("Было введено не число, попробуйте еще раз");
+    continue;
+  }
+  numbers.push(input);
+
+  for (const number of numbers) {
+    total += number;
+  }
+}
+console.log(numbers);
+console.log(`Общая сумма чисел равна - ${total}`);
