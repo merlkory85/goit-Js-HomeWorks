d7602fa31ba19983a33a96b62ce9af1b8afc7be4
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-02-19 21:58:59 +0200

    add Task_01

--- goit-js-hw-05/js/Task_01.js
+++ goit-js-hw-05/js/Task_01.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// Напиши функцию-конструктор Account, которая создает объект со свойствами login и email. В prototype
-// функции-конструктора добавь метод getInfo(), который выводит в консоль значения полей login и email объекта
-//  который его вызвал.
 const Account = function(name) {
   this.name = name;
   Account.prototype.getInfo = function() {
