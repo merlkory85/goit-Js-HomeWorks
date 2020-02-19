1cb4f2aec708030e0bb666e2dcdbe10d9c626c40
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-02-19 21:57:05 +0200

    make folder HOMEWORK_05

---
+++ goit-js-hw-05/js/Task_01.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Напиши функцию-конструктор Account, которая создает объект со свойствами login и email. В prototype
+// функции-конструктора добавь метод getInfo(), который выводит в консоль значения полей login и email объекта
+//  который его вызвал.
+const Account = function(name) {
+  this.name = name;
+  Account.prototype.getInfo = function() {
+    console.log(name);
+  };
+};
+
+console.dir(Account);
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: "Mangozedog",
+  email: "mango@dog.woof"
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: "Poly",
+  email: "poly@mail.com"
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
