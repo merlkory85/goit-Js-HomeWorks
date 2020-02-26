593dff91b3efcf6ed662df5590c0991ed184924f
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-02-26 23:09:18 +0200

    make folder HomeWork_06

---
+++ goit-js-hw-06/js/Task_01.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Получить массив имен всех пользователей (поле name).
+
+const getUserNames = users => {
+  // твой код
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
